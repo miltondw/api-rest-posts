bd3caa1ea82a4d32ceefded2ebd05b91080a9d18
miltondw <estradamilton2001@gmail.com>
2021-05-27 14:35:02 -0500

    Esctructur basic from server finished

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const morgan = require("morgan");
+const multer = require("multer");
+const path = require("path");
+//Initalizations
+const app = express();
+
+//Settings
+app.set("port", process.env.PORT || 3000);
+
+//Middlewares
+app.use(morgan("dev"));
+const storage = multer.diskStorage({
+  destination: path.join(__dirname, "public/uploads"),
+  filename(req, file, cb) {
+    cb(null, new Date().getTime() + path.extname(file.originalname));
+  },
+});
+app.use(multer({ storage }).single("image"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+//Start the server
+app.listen(app.get("port"), () => {
+  console.log("server on port", app.get("port"));
+});
