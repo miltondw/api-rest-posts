67517d82cc0d57a42940c0b4b8a085bd162110b3
miltondw <estradamilton2001@gmail.com>
2021-05-28 15:20:24 -0500

    change model by imagepath to type object

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require("mongoose");
 const postSchema = new Schema({
   title: { type: String, required: true },
-  imagePath: { type: String },
+  imagePath: { type: Object },
   paragraph: { type: Array },
   subtitle: { type: Array },
   type: { type: String, required: true },
